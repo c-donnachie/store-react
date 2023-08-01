dae397c7b36e1e05d77701973d79bf433847540d
Cristian Donnachie <119471458+c-donnachie@users.noreply.github.com>
2023-08-01 02:59:30 -0400

    Filters

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import { useState } from 'react'
 import Products from './components/Products'
-import {products} from './mocks/products.json'
+import {products as initialProducts} from './mocks/products.json'
 
 function App() {
+  const [products] = useState(initialProducts)
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0,
+  })
+
+  // Esto es para juniors :(
+  const filterProducts = (products) => {
+    return products.filter(product => {
+      return (
+        product.price >= filters.minPrice && (
+          filters.category === 'all' ||
+          product.category === filters.category
+         )
+       )
+    })
+  }
 
   return (
     <>
-      <Products products={products}/>
+      <Products products={filterProducts(products)}/>
     </>
   )
 }
