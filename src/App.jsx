5b65a197f1ec7fdb24e2636b8c9922c9f76a13d6
Cristian Donnachie <119471458+c-donnachie@users.noreply.github.com>
2023-08-01 02:19:59 -0400

    Primera carga

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Products from './components/Products'
+import {products} from './mocks/products.json'
+
+function App() {
+
+  return (
+    <>
+      <Products products={products}/>
+    </>
+  )
+}
+
+export default App
